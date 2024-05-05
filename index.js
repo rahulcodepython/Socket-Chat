074de0c71925d17b999934b7ef768c278025b625
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2024-05-06 02:04:51 +0530

    Project Init

---
+++ index.js
@@ -0,0 +1,27 @@
+import http from 'http';
+import express from 'express';
+import {Server} from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    },
+});
+
+const messages = []
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('send_message', (data) => {
+        messages.push(data)
+        io.emit('receive_message', data);
+    });
+});
+
+server.listen(9000, () => {
+    console.log('Server is running on port 9000');
+});
